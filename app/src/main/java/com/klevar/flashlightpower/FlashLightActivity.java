bd32a806c4553f94d58dc9534893eb8d86704f1e
Adminisrator <klkklevar@gmail.com>
2018-05-10 18:27:00 +0200

    Initial commit

---
+++ app/src/main/java/com/klevar/flashlightpower/FlashLightActivity.java
@@ -0,0 +1,171 @@
+package com.klevar.flashlightpower;
+
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.ImageSpan;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewParent;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+public class FlashLightActivity extends Activity implements SharedPreferences.OnSharedPreferenceChangeListener {
+    private static final String TAG = "FlashLightActivity";
+    private static final String LONG_PRESS = "long_press";
+    public static final String WHITE = "white";
+    private final Flash flash = new Flash();
+    private View background;
+    private ToggleButton theButton;
+    private Drawable dark;
+    private boolean changeColor = false;
+
+    private SharedPreferences sharedPreferences;
+
+    @SuppressLint("StaticFieldLeak")
+    public class FlashTask extends AsyncTask<Void, Void, Boolean> {
+        @Override
+        protected Boolean doInBackground(Void... voids) {
+            return flash.on();
+        }
+        @Override
+        protected void onPostExecute(Boolean success) {
+            theButton.setEnabled(true);
+            if (! success) {
+                Toast.makeText(FlashLightActivity.this, "Failed to access camera.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    public class WhiteTask extends AsyncTask<Void, Void, Boolean> {
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            return sharedPreferences.getBoolean(WHITE, false);
+        }
+
+        @Override
+        protected void onPostExecute(Boolean aBoolean) {
+            changeColor = aBoolean;
+            if (changeColor && theButton.isChecked()) {
+                background.setBackgroundColor(Color.WHITE);
+            } else {
+                background.setBackgroundDrawable(dark);
+            }
+        }
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    public class PressTask extends AsyncTask<Void, Void, Boolean> {
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            return sharedPreferences.getBoolean(LONG_PRESS, false);
+        }
+
+        @Override
+        protected void onPostExecute(Boolean aBoolean) {
+            background.setLongClickable(aBoolean);
+        }
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        theButton = (ToggleButton) findViewById(R.id.flashlightButton);
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        ViewParent vp = theButton.getParent();
+        if (vp instanceof View) {
+            background = (View) vp;
+            background.setOnLongClickListener(new LongClickListener());
+            dark = background.getBackground();
+        } else {
+            Log.e(TAG, "Background isn't a view!");
+            background = new View(this);
+        }
+
+        ImageSpan imageSpan = new ImageSpan(this, R.drawable.power_symbol);
+        SpannableString content = new SpannableString("X");
+        content.setSpan(imageSpan, 0, 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        theButton.setText(content);
+        theButton.setTextOn(content);
+        theButton.setTextOff(content);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        Log.i(TAG, "Changed pref: " + key);
+        switch (key) {
+            case LONG_PRESS:
+                new PressTask().execute();
+                break;
+            case WHITE:
+                new WhiteTask().execute();
+                break;
+        }
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        sharedPreferences.registerOnSharedPreferenceChangeListener(this);
+
+        if (theButton.isChecked()) {
+            theButton.setEnabled(false);
+            new FlashTask().execute();
+            theButton.setKeepScreenOn(true);
+        } else {
+            flash.off();
+        }
+
+        new PressTask().execute();
+        new WhiteTask().execute();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        sharedPreferences.unregisterOnSharedPreferenceChangeListener(this);
+        flash.close();
+    }
+
+    public void onToggleClicked(View v) {
+        if (theButton.isChecked()) {
+            new FlashTask().execute();
+            v.setKeepScreenOn(true);
+            if (changeColor) {
+                background.setBackgroundColor(Color.WHITE);
+            }
+        } else {
+            flash.off();
+            v.setKeepScreenOn(false);
+            if (background != null) {
+                background.setBackgroundDrawable(dark);
+            }
+        }
+    }
+
+    public class LongClickListener implements View.OnLongClickListener {
+        @Override
+        public boolean onLongClick(View view) {
+            theButton.setChecked(!theButton.isChecked());
+            onToggleClicked(theButton);
+            return true;
+        }
+    }
+}
